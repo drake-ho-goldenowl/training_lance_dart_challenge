60d69dd084bca9a8437151691474b36bdbfa5588
lance-go <lance.dinh.goldenowl@gmail.com>
2023-12-19 17:51:45 +0700

    Create project

---
+++ Dart Challange/unit_test_3/bin/unit_test_3.dart
@@ -0,0 +1,5 @@
+import 'package:unit_test_3/check_prime_number.dart' as unit_test_3;
+
+void main(List<String> arguments) {
+  unit_test_3.checkPrimeNumberFunc(number: 2);
+}
---
+++ Dart Challange/unit_test_3/test/unit_test_3_test.dart
@@ -0,0 +1,16 @@
+import 'package:unit_test_3/check_prime_number.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('Number 26', () {
+    expect(checkPrimeNumberFunc(number: 26), false);
+  });
+
+  test('Number 13', () {
+    expect(checkPrimeNumberFunc(number: 13), true);
+  });
+
+  test('Number 2', () {
+    expect(checkPrimeNumberFunc(number: 2), true);
+  });
+}
