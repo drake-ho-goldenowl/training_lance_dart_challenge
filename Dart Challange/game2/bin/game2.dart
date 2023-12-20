8e9818b7f9d9be5cd5084bd37e966179804cf137
lance-go <lance.dinh.goldenowl@gmail.com>
2023-12-20 10:54:36 +0700

    Complete code

--- Dart Challange/game2/bin/game2.dart
+++ Dart Challange/game2/bin/game2.dart
@@ -1,5 +1,5 @@
 import 'package:game2/game2.dart' as game2;
 
 void main(List<String> arguments) {
-  print('Hello world: ${game2.calculate()}!');
+  game2.gameStart();
 }
--- Dart Challange/game2/lib/game2.dart
+++ Dart Challange/game2/lib/game2.dart
@@ -3,10 +3,27 @@ import 'dart:math';
 
 void gameStart() {
   while (true) {
-    stdout.write("Type 'exit' to quit the game");
+    stdout.writeln("Type 'exit' to quit the game");
+    int count = 1;
     int randomValue = createRandomNumber();
-    String userInputValue = getUserInput();
-    if (userInputValue)
+    String userInput = getUserInput();
+    if (userInput.compareTo('exit') == 0) return;
+
+    int userInputValue = int.parse(userInput);
+    while (userInputValue != randomValue) {
+      if (userInputValue > randomValue) {
+        stdout.writeln("You are higher");
+      } else if (userInputValue < randomValue) {
+        stdout.writeln("You are lower");
+      }
+      count++;
+      userInput = getUserInput();
+      if (userInput.compareTo('exit') == 0) return;
+      userInputValue = int.parse(userInput);
+    }
+    stdout.writeln("Bingo! You tried $count times \n");
+
+    stdout.writeln("Play again!");
   }
 }
 
@@ -17,7 +34,19 @@ int createRandomNumber() {
 }
 
 String getUserInput() {
-  stdout.write("Please choose a number between 0 and 100:");
-  String? userInput = stdin.readLineSync();
-  return userInput ?? '';
+  RegExp invalidInput = RegExp(r'[a-zA-Z]|[^\w\s]');
+  String? userInput;
+  do {
+    stdout.write("Please choose a number between 0 and 100:");
+    userInput = stdin.readLineSync();
+    if (userInput != null &&
+        (userInput.compareTo('exit') == 0 ||
+            !invalidInput.hasMatch(userInput))) {
+      if (userInput.compareTo('exit') == 0) break;
+
+      int userNumber = int.parse(userInput);
+      if (userNumber <= 100 && userNumber >= 0) break;
+    }
+  } while (true);
+  return userInput;
 }
