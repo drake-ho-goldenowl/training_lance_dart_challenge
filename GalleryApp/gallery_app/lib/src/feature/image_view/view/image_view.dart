51cad321552065acbb68a95a8dfd44b7afa8e35f
DeathA2 <dinhluong13579@gmail.com>
2024-01-02 14:58:05 +0700

    Change feature

--- GalleryApp/gallery_app/lib/src/feature/image_view/view/image_view.dart
+++ GalleryApp/gallery_app/lib/src/feature/image_view/view/image_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:gallery_app/src/config/constant/link_manager.dart';
-import 'package:gallery_app/src/config/constant/text_manager.dart';
 import 'package:gallery_app/src/config/constant/value_manager.dart';
 
 class ImageView extends StatelessWidget {
@@ -16,12 +15,13 @@ class ImageView extends StatelessWidget {
             flexibleSpace: FlexibleSpaceBar(
               background: _renderSliverAppBar(),
               stretchModes: const [StretchMode.zoomBackground],
+              title: _renderInformationTitle(),
             ),
             pinned: true,
             expandedHeight: 200,
             stretch: true,
             elevation: 0,
-            title: const Text(AppString.imageView),
+            centerTitle: false,
           ),
           SliverList(
             delegate: SliverChildBuilderDelegate(
@@ -36,16 +36,6 @@ class ImageView extends StatelessWidget {
   }
 
   Widget _renderSliverAppBar() {
-    return Stack(
-      fit: StackFit.expand,
-      children: [
-        _renderImage(),
-        _renderInformationTitle(),
-      ],
-    );
-  }
-
-  Widget _renderImage() {
     return Image.network(
       AppLinkManager.luffyLinkImage,
       fit: BoxFit.cover,
@@ -53,43 +43,14 @@ class ImageView extends StatelessWidget {
   }
 
   Widget _renderInformationTitle() {
-    return Positioned(
-      left: AppPadding.p10,
-      bottom: AppPadding.p0,
-      child: Opacity(
-        opacity: 1,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "Monkey D. Luffy",
-              style: TextStyle(
-                  color: Colors.white,
-                  letterSpacing: 2,
-                  backgroundColor: Colors.black.withOpacity(0.5),
-                  fontSize: AppFontSize.f20,
-                  fontWeight: FontWeight.w700),
-            ),
-            _renderInforChip(),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _renderInforChip() {
-    return Row(
-      children: [
-        Chip(
-          label: const Text('Tu Hoang'),
-          backgroundColor: Colors.grey[350],
-        ),
-        const SizedBox(width: AppPadding.p16),
-        Chip(
-          label: const Text('Than Mat Troi NiKa'),
-          backgroundColor: Colors.grey[350],
-        ),
-      ],
+    return Text(
+      "Monkey D. Luffy",
+      style: TextStyle(
+          color: Colors.white,
+          letterSpacing: 2,
+          backgroundColor: Colors.black.withOpacity(0.5),
+          fontSize: AppFontSize.f20,
+          fontWeight: FontWeight.w700),
     );
   }
 }
